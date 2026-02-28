57f7dd75a7ae24aafc685db4039c02dc5a75fc3a
DrGalan <>
2026-02-28 15:10:12 +0000

    Updates

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'lingualens-v1';
+const ASSETS_TO_CACHE = [
+  '/mymlapp/',
+  '/mymlapp/index.html',
+  '/mymlapp/manifest.json',
+  '/mymlapp/icon.svg',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        if (response.ok) {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match('/mymlapp/index.html')))
+  );
+});
